80cf9363b98a2a67254ebb23739280f4ec34620f
Erik Thalén <erik.thalen@hyperisland.se>
2024-04-10 10:47:40 +0200

    Update use-refs.js

--- frontend/framework/use-refs.js
+++ frontend/framework/use-refs.js
@@ -1,66 +1,68 @@
 /**
  * 
  * @param {HTMLElement} [target=document.body] - Element to querySelect in
+ * @param {boolean} [namespaced] - Only get children with data-ref-parent-ref
  * @param {string} [exclude] - Selector or element who's children wont be selected
  * @param {boolean | function} [watch] - Watches DOM and updates refs on mutation
  * @param {boolean} [asArray] - Saves all refs in arrays, also single elements
  * @returns {Object} { myRef: div, another: [button, span] }
  */
-export default ({ target = document.body, exclude, watch, asArray } = {}) => {
-  const root =
-    typeof target === 'string'
-      ? document.querySelector(`[data-namespace="${target}"]`)
-      : target
-  const name = root?.getAttribute('data-namespace')
-
-  return watch
-    ? _watched(root, name, watch, asArray, exclude)
-    : _refs(root, name, true, asArray, exclude)
-}
-
-function _refs(root, name, includeRoot, asArray, exclude) {
-  const camelCase = s => s.replace(/-./g, x => x[1].toUpperCase())
-  const capitalize = s => s.charAt(0).toUpperCase() + s.slice(1)
-  const attribute = name ? `data-ref-${name}` : 'data-ref'
-
-  return [...root.querySelectorAll(`[${attribute}]`)].reduce((refs, el) => {
-    if (el.closest(exclude))
-      return {
-        ...(includeRoot && { root }),
-        ...refs,
-      }
-
-    const dataset = name ? 'ref' + capitalize(camelCase(name)) : 'ref'
-    return {
-      ...(includeRoot && { root }),
-      ...refs,
-      ...(camelCase(el.dataset[dataset]) || 'item').split(' ').reduce(
-        (acc, newRef) => ({
-          ...acc,
-          [newRef]: refs[newRef]
-            ? [refs[newRef], el].flat()
-            : asArray
-            ? [el]
-            : el,
-        }),
-        {}
-      ),
-    }
-  }, {})
-}
-
-function _watched(r, n, onChange, asArray, exclude) {
-  const observer = new MutationObserver(() => {
-    ref.current = _refs(r, n)
-    typeof onChange === 'function' && onChange()
-  })
-  observer.observe(r, { childList: true, subtree: true })
-
-  const ref = {
-    root: r,
-    current: _refs(r, n, true, asArray),
-    unwatch: observer.disconnect.bind(observer),
+export default ({
+  root = document.body,
+  namespaced = false,
+  watch = false,
+  asArray = false,
+  exclude = null,
+}: any = {}) => {
+  if (!(root instanceof HTMLElement)) {
+    console.warn('{ root } has to be an element')
+    return
   }
 
-  return ref
+  return {
+    root,
+    ...(watch
+      ? _watched(root, watch, exclude, asArray, namespaced)
+      : _refs(root, exclude, asArray, namespaced))
+  }
+}
+
+function _refs(root: HTMLElement, exclude = null, asArray = false, namespaced = false) {
+  const camelCase = (s: string | undefined) => s ? s.replace(/-./g, x => x[1].toUpperCase()) : ''
+  const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
+
+  const attribute = namespaced ? `data-ref-${root.dataset.ref}` : 'data-ref'
+  const elements = root.querySelectorAll(`[${attribute}]`)
+
+  const refs = new Map()
+
+  elements.forEach(element => {
+    if (exclude && element.closest(exclude)) return
+
+    const dataset = 'ref' + (namespaced ? capitalize(camelCase(root.dataset.ref)) : '')
+    const keys = (camelCase((element as HTMLElement).dataset[dataset]) || 'item').split(' ')
+
+    keys.forEach(key => {
+      if (refs.has(key)) {
+        refs.set(key, [refs.get(key), element].flat())
+      } else {
+        refs.set(key, asArray ? [element] : element)
+      }
+    })
+  })
+
+  return Object.fromEntries(refs)
+}
+
+function _watched(root: HTMLElement, onChange: ((args?: any) => any) | boolean, ...args: any[]) {
+  const ref = { current: _refs(root, ...args) }
+
+  const observer = new MutationObserver(() => {
+    ref.current = _refs(root, ...args)
+    typeof onChange === 'function' && onChange(ref.current)
+  })
+
+  observer.observe(root, { childList: true, subtree: true })
+
+  return { ref, unwatch: observer.disconnect.bind(observer) }
 }
